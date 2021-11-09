4c82544cd46e49417ed521313e01a0386e364c36
Ysh1da <92174112+Ysh1da@users.noreply.github.com>
2021-11-09 18:45:32 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Library;
+
+namespace ConsoleApp
+
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("First Player name");
+            string readName = Console.ReadLine();
+            Console.WriteLine("HP");
+            int readHealth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Damage");
+            int readDamage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kevlar");
+            int readArmour = Convert.ToInt32(Console.ReadLine());
+            Character FirstPlayer = new Character(readName, readHealth, readDamage, readArmour);
+            Console.WriteLine("Player stats: " + FirstPlayer.ToString());
+
+            Console.WriteLine("First Player name");
+            readName = Console.ReadLine();
+            Console.WriteLine("HP");
+            readHealth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Damage");
+            readDamage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kevlar");
+            readArmour = Convert.ToInt32(Console.ReadLine());
+            Character SecondPlayer = new Character(readName, readHealth, readDamage, readArmour);
+            Console.WriteLine("Player stats: " + SecondPlayer.ToString());
+
+        }
+    }
+}
